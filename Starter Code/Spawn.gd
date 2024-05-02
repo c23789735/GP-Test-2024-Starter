21b1e0f97b0f1331bc1189a0a5e9a1cc4f49c13f
Yuki-not-cutie <c23789735@mytudublin.ie>
2024-05-02 13:15:54 +0100

    script

--- Starter Code/Spawn.gd
+++ Starter Code/Spawn.gd
@@ -1,5 +1,6 @@
 extends Button
 
+var  number:float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,9 +8,8 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
 
 
 func _on_h_slider_value_changed(value):
-	pass # Replace with function body.
+	number = value
